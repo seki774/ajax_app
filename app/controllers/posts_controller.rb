ac6a763b086a425a34aad5ff339f01fd71505221
seki774 <75604754+seki774@users.noreply.github.com>
2020-12-21 16:54:51 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,6 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")  
   end
-
-  
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
