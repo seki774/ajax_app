46e09dfbc036489d88ae278538954ba9a8e039a8
seki774 <75604754+seki774@users.noreply.github.com>
2020-12-21 11:41:38 +0900

    AjaxAppの準備

---
+++ app/javascript/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("lists")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    console.log("mouseout OK")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    console.log("click OK")
+  })
+})
